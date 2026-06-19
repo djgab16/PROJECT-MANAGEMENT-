9257dd8cac1935a32cdaf01a6efb2b157ac8623a
djgab16 <gabrieldavid0516@gmail.com>
2026-06-20 02:48:16 +0800

    chore: generate build artifacts and temporary compilation files for .NET 10.0 project

--- Delivery_System_Capstone_API/obj/Debug/net10.0/SPXDeliveryAPI.AssemblyInfo.cs
+++ Delivery_System_Capstone_API/obj/Debug/net10.0/SPXDeliveryAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SPXDeliveryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb608beb277bc603849f952cf13a29b6d41632f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7ebc6169eb351d0ff2aa268c3f26ec418e65488")]
 [assembly: System.Reflection.AssemblyProductAttribute("SPXDeliveryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SPXDeliveryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
